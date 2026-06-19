deef8f1c839d5bb752d3083c2dd5dc9e9445782d
Damon Anderson <damon@h3dgamma.com>
2026-06-19 10:26:48 -0400

    Pointing build script at my tool commit for local builds

--- build_openroad.sh
+++ build_openroad.sh
@@ -15,8 +15,8 @@ cd "$DIR"
 # Defaults variable values
 NICE=""
 
-OPENROAD_APP_REMOTE="origin"
-OPENROAD_APP_BRANCH="master"
+OPENROAD_APP_REMOTE="myfork"
+OPENROAD_APP_BRANCH="add-multicut-basic-support"
 
 INSTALL_PATH="$(pwd)/tools/install"
 
@@ -29,7 +29,7 @@ OPENROAD_APP_ARGS=""
 DOCKER_OS_NAME="ubuntu22.04"
 PROC=-1
 
-WITH_VERIFIC=0
+WITH_VERIFIC=""
 VERIFIC_SRC=""
 VERIFIC_COMPONENTS='database util containers pct hier_tree verilog'
 VERIFIC_ARGS=" VERIFIC_COMPONENTS='${VERIFIC_COMPONENTS}'"
@@ -243,7 +243,7 @@ __docker_build()
                 options="-buildArgs=--build-arg verificPath=tools/verific"
         fi
         ./etc/DockerHelper.sh create -target=dev -os="${DOCKER_OS_NAME}" -threads="${PROC}"
-        ./etc/DockerHelper.sh create -target=builder -os="${DOCKER_OS_NAME}" -threads="${PROC}" "${options}"
+        ./etc/DockerHelper.sh create -target=builder -os="${DOCKER_OS_NAME}" -threads="${PROC}"
         rm -rf tools/verific
         if [ ! -z "${DOCKER_COPY_PLATFORMS+x}" ]; then
                 mv .dockerignore{.bak,}
